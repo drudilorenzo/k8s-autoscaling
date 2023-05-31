708c5ffc356181aa9a7e21d0d0ec4468e1aca8b6
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-05-31 10:20:13 +0200

    Improve load generator.

--- vertical-pod-autoscaler/RUN_LOAD_GENERATOR.sh
+++ vertical-pod-autoscaler/RUN_LOAD_GENERATOR.sh
@@ -1,1 +1,1 @@
-kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -O - http://172.17.34.60:80; done"
+kubectl run -i --tty load-generator --rm --image=busybox:1.28 -- /bin/sh -c "while sleep 0.01; do wget -O- -q http://apache-service; done"
--- vertical-pod-autoscaler/START_APP_AND_RUN_LOAD_GENERATOR.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./START_APP.sh
-
-IP=`kubectl get service apache-service | awk 'NR==2 {print $3}'`;
-
-kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -O - http://${IP}:80; done"
