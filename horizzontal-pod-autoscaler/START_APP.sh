70857db8bce5e9bf182f591ce2a7dddca6f1bbe0
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-06-02 19:20:18 +0200

    Fix load generator.

--- horizzontal-pod-autoscaler/START_APP.sh
+++ horizzontal-pod-autoscaler/START_APP.sh
@@ -3,11 +3,9 @@
 kubectl apply -f php-apache.yaml     && \
 kubectl get service php-apache
 
-if (( $? == 0 )) ; then echo ok ; 
-
-kubectl get service php-apache | grep php-apache | ( \
-	read A B IP D E F ;
-    echo service at  http://${IP}:80/
- )
- 
-else		echo errore ; fi
+if (( $? == 0 ))
+then
+    echo ok
+else
+    echo errore
+fi
--- vertical-pod-autoscaler/RUN_LOAD_GENERATOR.sh
+++ vertical-pod-autoscaler/RUN_LOAD_GENERATOR.sh
@@ -1,1 +1,1 @@
-kubectl run -i --tty load-generator --rm --image=busybox:1.28 -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://172.17.3.191:80/; done"
+kubectl run -i --tty load-generator --rm --image=busybox:1.28 -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://apache-service; done"
